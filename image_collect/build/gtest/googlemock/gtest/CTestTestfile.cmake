5fb3693a5d0e686dd992e31c92772e8475791afa
Z-Jeff <1490969160@qq.com>
2020-11-25 18:02:08 +0800

    3

--- image_collect/build/gtest/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googletest
-# Build directory: /home/jeff/project/RGBD_annotation/image_collect/build/gtest/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
